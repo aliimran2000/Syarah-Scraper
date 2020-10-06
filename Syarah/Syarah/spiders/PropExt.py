c245171538ec30eab8175ecaf8f49613bfcbc359
ali <ali@pop-os.localdomain>
2020-10-07 00:18:03 +0500

    done xpath of non links

--- Syarah/Syarah/spiders/PropExt.py
+++ Syarah/Syarah/spiders/PropExt.py
@@ -10,6 +10,64 @@ def containsdigit(stringval):
     return False
 
 
+def identifyfield(value):
+    if ('الماركة' in value):
+        return 'brand'
+
+    if ('الموديل'in value):
+        return 'model'
+
+    if ('نوع الوقود'in value):
+        return 'fueltype'
+    
+    if ('اللون'in value):
+        return 'color'
+    
+    if ('الفئه'in value):
+        return 'category'
+
+
+    if ('الممشى'in value):
+        return 'walkaway'
+
+    if ('نوع القير'in value):
+        return 'geartype'
+
+    if ('المواصفات'in value):
+        return 'specifications'
+
+    if ('سعة المحرك' in value):
+        return 'enginecapacity'
+    
+    if ('النوع' in value):
+        return 'type'
+    
+
+    return value
+
+
+
+#Type |النوع :
+
+#model|الموديل :
+
+#fueltpye |نوع الوقود :
+
+#color |اللون:
+
+#Category:|الفئه:
+
+#price 
+
+#walkawayt|الممشى:
+
+#geartype|نوع القير :
+
+#sepcifications |المواصفات:
+
+#engine capacity |سعة المحرك:
+
+
 
 class PropextSpider(scrapy.Spider):
     name = 'PropExt'
@@ -24,8 +82,8 @@ class PropextSpider(scrapy.Spider):
         #for obj in data:
         self.start_urls.pop()
 
-        for i in range(0,int(len(data)/100)):
-            self.start_urls.append(data[i]['url'])
+        #for i in range(0,int(len(data)/100)):
+        self.start_urls.append(data[7]['url'])
 
 
 
@@ -33,15 +91,60 @@ class PropextSpider(scrapy.Spider):
         title = response.xpath('//div[@class="container"]/div[@class="clearfix main_titles_box post_title"]/h1/span/text()').get()
         price = response.xpath('//div[@class="container"]/div[@class="clearfix main_titles_box post_title"]/span/i/text()').get()
 
-        if containsdigit(price):
-            yield {
-                'title':title,
-                'price':price
-            }
-        else:
-            yield {
-                'title':title,
-                'price':None,
-            }
+        if (containsdigit(price)) == False:
+            price = None
 
-$x('//div[@class="row post_ad_list_details"]/div[@class="col-lg-6 col-md-6 col-sm-12 col-xs-12"]/p/span/font/font')
+        data = response.xpath('//div[@class="row post_ad_list_details"]/div[@class="col-lg-6 col-md-6 col-sm-12 col-xs-12"]')
+        Car={}
+        Car['title'] = title
+        Car['price'] = price
+
+
+        for obj in data:
+            key = obj.xpath('.//p/span[@class="title"]/text()').get()   
+            val = obj.xpath('.//p/span[@itemprop]/text()').get()        
+
+            yield{
+                'key':identifyfield(key),
+                'val':val
+            }
+                
+
+
+
+            #if (identifyfield(all) == 'fueltype'):
+            #    response.xpath('//div[@class="row post_ad_list_details"]/div[@class="col-lg-6 col-md-6 col-sm-12 col-xs-12"]/p/span[@class="title"]/text()').getall()
+
+
+            
+
+        #yield {
+        #    'title':title,
+        #    'price':price
+        #}
+      
+
+
+
+#tital
+#Brand |الماركة :  
+
+#Type |النوع :
+
+#model|الموديل :
+
+#fueltpye |نوع الوقود :
+
+#color |اللون:
+
+#Category:|الفئه:
+
+#price 
+
+#walkawayt|الممشى:
+
+#geartype|نوع القير :
+
+#sepcifications |المواصفات:
+
+#engine capacity |سعة المحرك:
